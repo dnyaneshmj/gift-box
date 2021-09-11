7de9efa41389c8ae70fe41b3a64caee6360eef09
Dnyanesh <dnyanesh.mahajan@predikly.com>
2021-09-12 01:26:53 +0530

    cart variable

--- templates/mini-cart.php
+++ templates/mini-cart.php
@@ -313,7 +313,9 @@ $opt_header_shopping_cart_button = g5plus_get_option('header_shopping_cart_butto
 
                                 $_product   = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
                                 $product_id = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
-                                
+                                $product_price     = apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key );
+                                $product_permalink = apply_filters( 'woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink( $cart_item ) : '', $cart_item, $cart_item_key );
+                                $sku = $_product->get_sku();
                                 $has_greeting = (get_post_meta( $product_id, 'is_greeting_card', true ) == 'true')? true : false;
 
                                 ?>
@@ -352,7 +354,7 @@ $opt_header_shopping_cart_button = g5plus_get_option('header_shopping_cart_butto
                                                     echo wp_kses_post( apply_filters( 'woocommerce_cart_item_backorder_notification', '<p class="backorder_notification">' . esc_html__( 'Available on backorder', 'g5plus-handmade' ) . '</p>' ) );
                                                 }
                                             ?>
-                                    <p class="item-sku"> <b>SKU :</b> <span> ER345</span> </p>
+                                    <p class="item-sku"> <b>SKU :</b> <span> <?php echo $sku ?></span> </p>
                                     </div>
                                     
                                     <div class="gf-item-count product-quantity">
